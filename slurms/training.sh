10b4a593fd1063da70ee5b8b7c995a6cce77ed65
arikalinsky14 <arikalinsky14@gmail.com>
2026-05-24 21:11:42 -0400

    changed time limit

--- slurms/training.sh
+++ slurms/training.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --qos=short
 #SBATCH --mail-user=ajk245@pitt.edu
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
